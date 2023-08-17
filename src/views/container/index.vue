2e330975dac229b066a18eb05bab09ea75a3c0a2
ArthurWang <legendaryarthur@163.com>
2023-08-18 00:45:30 +0800

    feat: 页面布局

---
+++ src/views/container/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Chat from '../chat/index.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="nav">
+      nav
+    </div>
+    <div class="content">
+      <Chat />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.nav {
+  width: 120px;
+  background-color: bisque;
+  height: 100%;
+}
+
+.content {
+  flex: 1;
+}
+</style>
